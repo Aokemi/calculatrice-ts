0b1deb47b33535a98a6984c97cdda32446cc7ad8
Aokemi <audrin.yann@gmail.com>
2024-10-28 18:13:48 +0100

    Started creating converter page

--- src/Pages/Convertisseur.tsx
+++ src/Pages/Convertisseur.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 
 
 const Convertisseur = () => {
     
+    const [temperature, setTemperature] = useState<number>(0)
+    const [volume, setVolume] = useState<number>(0)
+    const [temps, setTemps] = useState<string>("")
+
+    const handleChange = (e: any) => {
+        if (e.target.name === "temperature"){
+            setTemperature(e.target.value)
+        } else if (e.target.name === "volume"){
+            setVolume(e.target.value)
+        } else if (e.target.name === "temps"){
+            setTemps(e.target.value)
+        }
+    }
     
 
     return (
         <div>
-                
+            <p>Choisissez le type de conversion : </p>
+            <select id="convert" name="convert" onChange={handleChange}>
+                <option value="temperature"> {temperature} </option>
+                <option value="volume"> {volume} </option>
+                <option value="temps"> {temps} </option>
+            </select>
         </div>
     );
     
