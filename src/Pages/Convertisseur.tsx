91104041d50b596fbaca293863a56dd319b05b4f
Aokemi <audrin.yann@gmail.com>
2024-10-29 14:22:50 +0100

    Created the first two inputs for a one-way temperature conversion

--- src/Pages/Convertisseur.tsx
+++ src/Pages/Convertisseur.tsx
@@ -7,7 +7,7 @@ const Convertisseur = () => {
     const [volume, setVolume] = useState<number>(0)
     const [temps, setTemps] = useState<string>("")
 
-    const handleChange = (e: any) => {
+    const handleChange = (e: React.ChangeEvent<any>) => {
         if (e.target.name === "temperature"){
             setTemperature(e.target.value)
         } else if (e.target.name === "volume"){
@@ -16,16 +16,35 @@ const Convertisseur = () => {
             setTemps(e.target.value)
         }
     }
+
+    const handleChangeValue = (e : React.ChangeEvent<any> ) => {
+
+        setTemperature(e.target.value)
+
+    }
+
+    function convertTemperature(number: number){
+        const converted = (number - 32) * 5 / 9
+        return converted
+    }
     
 
     return (
         <div>
             <p>Choisissez le type de conversion : </p>
             <select id="convert" name="convert" onChange={handleChange}>
-                <option value="temperature"> {temperature} </option>
-                <option value="volume"> {volume} </option>
-                <option value="temps"> {temps} </option>
+                <option value="temperature"> Température </option>
+                <option value="volume"> Volume </option>
+                <option value="temps"> Temps </option>
             </select>
+
+            < br />
+            < br />
+            <input type="number" name="temperature" value={temperature} onChange={handleChangeValue}/>
+            <input type="number" name="convertTemperature" value={convertTemperature(temperature)}/>
+            < br />
+
+
         </div>
     );
     
