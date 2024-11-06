5168fe86529892c25ecbd63e34945b0930e85c4e
Aokemi <audrin.yann@gmail.com>
2024-11-06 22:26:10 +0100

    Converter eventually working for three types

--- src/Pages/Convertisseur.tsx
+++ src/Pages/Convertisseur.tsx
@@ -1,41 +1,33 @@
 import { useState } from "react";
+import ConvertTemperature from "../Components/convertTemperature";
+import ConvertVolume from "../Components/convertVolume";
+import ConvertTemps from "../Components/ConvertTemps";
 
 const Convertisseur = () => {
     
-    const [inputTop, setInputTop] = useState<string>("")
-    const [inputBottom, setInputBottom] = useState<string>("")
+    const [conversionType, setConversionType] = useState<string>("temperature")
 
-    function celsiusToFarenheit(celsius: string){
-        const value = (parseFloat(celsius) * 9 / 5 + 32).toFixed(2)
-        return value
-    }    
-    
-    function farenheitToCelsius(farenheit: string){
-        const value = (parseFloat(farenheit) - 32 * 5 / 9).toFixed(2)
-        return value
-    }
+    const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) =>  {
 
-    const handleChangeTop = (e: React.ChangeEvent<HTMLInputElement>) =>  {
-
-        setInputTop(e.target.value) // On charge la valeur du haut
-        setInputBottom(celsiusToFarenheit(e.target.value)) // Puis on fait la conversion en bas
-
-    }
-
-    const handleChangeBottom = (e: React.ChangeEvent<HTMLInputElement>) => {
-
-        setInputBottom(e.target.value)
-        setInputTop(farenheitToCelsius(e.target.value))
+        setConversionType(e.target.value)
 
     }
 
     return (
         <div>
             <h2> Choisissez le type de conversion : </h2>
-            <input type="number" value={inputTop} onChange={handleChangeTop}/> 
-            <p> = </p>
-            <input type="number" value={inputBottom} onChange={handleChangeBottom}/>
-        </div>
+
+            <select onChange={handleSelect} value={conversionType}>
+                <option value="temperature"> Température </option>
+                <option value="volume"> Volume </option>
+                <option value="temps"> Temps </option>
+            </select>
+
+            {conversionType === "temperature" && <ConvertTemperature />}
+            {conversionType === "volume" && <ConvertVolume />}
+            {conversionType === "temps" && <ConvertTemps />}
+            
+        </div> 
     );
     
 }
